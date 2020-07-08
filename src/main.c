7abbada0e29f1aa4ac481bd3dccfb2ddb8e44fb5
TheGuyMatt <matthew.hieb@protonmail.com>
2020-07-08 13:48:07 -0700

    Added player draw function

--- src/main.c
+++ src/main.c
@@ -1,10 +1,21 @@
+#include <SDL2/SDL_render.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <SDL2/SDL.h>
 
+SDL_Window *window;
+SDL_Renderer *renderer;
 #define WINDOW_WIDTH 1024
 #define WINDOW_HEIGHT 512
+
+float px, py; //player position
+void draw_player()
+{
+  SDL_Rect rect = {px, py, 8, 8};
+  SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+  SDL_RenderFillRect(renderer, &rect);
+}
 
 int process_events(SDL_Event *event)
 {
@@ -29,21 +40,30 @@ void render(SDL_Renderer *renderer)
   SDL_RenderClear(renderer);
 
   //draw
+  draw_player();
     
   //present
   SDL_RenderPresent(renderer);
 }
 
-int main()
+void init()
 {
   //init sdl
   SDL_Init(SDL_INIT_VIDEO);
 
   //create sdl window
-  SDL_Window *window = SDL_CreateWindow("RaycastEngine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
+  window = SDL_CreateWindow("RaycastEngine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL);
 
   //create renderer
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+  px = 300; py = 300; //init player position
+}
+
+int main()
+{
+  //initialize game
+  init();
 
   int running = 1; 
   SDL_Event event;
